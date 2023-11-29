cca61ed395bd80480d23f6fb8a2281256f151acc
Roboterjiang <roboterjiang@163.com>
2023-11-29 15:26:51 +0800

    修改版本号

--- dist/cornerstone.js
+++ dist/cornerstone.js
@@ -1,4 +1,4 @@
-/*! cornerstone-core - 0.0.0-semantically-released - 2023-11-29 | (c) 2016 Chris Hafey | https://github.com/cornerstonejs/cornerstone */
+/*! cornerstone-core - 1.0.0-semantically-released - 2023-11-29 | (c) 2016 Chris Hafey | https://github.com/cornerstonejs/cornerstone */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
